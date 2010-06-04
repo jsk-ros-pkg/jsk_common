3554c5322a8fe6951f5735b412b83ff6df18c457
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2010-06-04 15:42:17 +0000

    introduce new stack jsk_coordination_system and new package dynamic_tf_publisher

---
+++ dynamic_tf_publisher/src/tf_publish.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# publish static tf which is set by SetDynamicTF service
+# publishing tf is uniquely by child_frame_id
+#
+# TODO: delete target tf
+#       check tf tree consistency
+#       change frequency by frame_id
+#
+import roslib; roslib.load_manifest('dynamic_tf_publisher')
+import rospy
+
+from dynamic_tf_publisher.srv import * # SetDynamicTF
+from geometry_msgs.msg import TransformStamped
+import tf
+
+class dynamic_tf_publisher:
+    def __init__(self):
+        self.cur_tf = dict()
+        self.tf_sleep_time = 1
+        rospy.Service('/set_dynamic_tf', SetDynamicTF, self.set_tf)
+
+    def publish_tf(self,frame_id):
+        pose = self.cur_tf[frame_id]
+        br = tf.TransformBroadcaster()
+        pos = pose.transform.translation
+        rot = pose.transform.rotation
+        br.sendTransform((pos.x, pos.y, pos.z),
+                         (rot.x, rot.y, rot.z, rot.w),
+                         rospy.Time.now(),
+                         pose.child_frame_id,
+                         pose.header.frame_id)
+
+    def set_tf(self,req):
+        print "Latch [%s]"%(req.cur_tf.child_frame_id)
+        tf_sleep_time = 1.0/req.freq
+        self.cur_tf[req.cur_tf.child_frame_id] = req.cur_tf
+        self.publish_tf(req.cur_tf.child_frame_id)
+        return SetDynamicTFResponse()
+
+    def publish_and_sleep(self):
+        map(self.publish_tf, self.cur_tf)
+        rospy.sleep(self.tf_sleep_time)
+
+if __name__ == "__main__":
+    rospy.init_node('tf_latch_server')
+    pub = dynamic_tf_publisher()
+    while not rospy.is_shutdown():
+        pub.publish_and_sleep()
+
---
+++ dynamic_tf_publisher/src/tf_set.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('dynamic_tf_publisher')
+
+import sys
+import rospy
+import tf
+from dynamic_tf_publisher.srv import SetDynamicTF
+from geometry_msgs.msg import *
+
+def set_tf(pos, q, parent, child, freq):
+    rospy.wait_for_service('/set_dynamic_tf')
+    try:
+        client = rospy.ServiceProxy('/set_dynamic_tf', SetDynamicTF)
+        pose = TransformStamped()
+        pose.header.frame_id = pa
+        pose.child_frame_id = ch
+        pose.transform.translation = Vector3(*pos)
+        pose.transform.rotation = Quaternion(*q)
+        print pose
+        res = client(freq, pose)
+        return
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def isfloat(str):
+    try:
+        float(str)
+        return True
+    except Exception, e:
+        return False
+
+if __name__ == "__main__":
+    try:
+        pos = map(float,sys.argv[1:4])
+        if isfloat(sys.argv[7]): # quaternion
+            q = map(float,sys.argv[4:8])
+            rest = sys.argv[8:]
+        else: # rpy
+            rpy = map(float,sys.argv[4:7])
+            # rpy angle is in "zyx" axis
+            q = tf.transformations.quaternion_from_euler(*rpy, axes="rzyx")
+            print q
+            rest = sys.argv[7:]
+        pa = rest[0]
+        ch = rest[1]
+        hz = 1000.0 / float(rest[2])
+        set_tf(pos, q, pa, ch, hz)
+    except Exception, e:
+        print sys.argv
+        print "args: x y z (r p y)|(x y z w) parent child msec"
+    sys.exit(0)
