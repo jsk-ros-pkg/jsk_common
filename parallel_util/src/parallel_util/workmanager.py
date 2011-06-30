cab77cdae787a9cd5452bc7596a16ed6a9def39a
rosen <rosen@jsk.t.u-tokyo.ac.jp>
2011-06-30 05:24:19 +0000

    added parallelization worker

---
+++ parallel_util/src/parallel_util/workmanager.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Rosen Diankov <rosen.diankov@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Managers multiple processors running on the ROS network completing a task.
+
+The manager takes in a module that provides and processes the work items, it is oblivious of the ROS network. The module needs to provide the following functions:
+
+.. code-block:: 
+  # service side
+  def service_start(args):
+
+  # service side
+  def service_processrequest(request):
+
+  # server side
+  def server_processresponse(response):
+
+  # server side
+  def server_requestwork():
+
+  # server side
+  def server_start(argv):
+
+  # server side
+  def server_end(argv):
+
+  # launcher initialization
+  def launcher_start(argv):
+
+
+There are 3 modes when launching the worker: initial launch setup, server manager, service runner.
+
+"""
+import time
+import inspect
+import os, sys
+import threading
+from optparse import OptionParser
+
+from srv import PickledService, PickledServiceRequest, PickledServiceResponse
+from cpuinfo import cpuinfos
+
+class EvaluationServerThread(threading.Thread):
+    def __init__(self, service, finishcb):
+        threading.Thread.__init__(self)
+        self.service = service
+        self.finishcb = finishcb
+        self.ok = True
+        self.starteval = threading.Condition(threading.Lock())
+        self.req = None
+
+    def run(self):
+        with self.starteval:
+            while self.ok:
+                self.starteval.wait()
+                if not self.ok:
+                    break
+                if self.req is None:
+                    rospy.logwarn('dummy command?')
+                    continue
+                res = self.service(self.req)
+                if res is not None and self.ok:
+                    self.finishcb(pickle.loads(res.output))
+                self.req = None
+
+class EvaluationServer(object):
+    def __init__(self,module,servicenames):
+        self.evallock = threading.Lock()
+        self.allthreads = []
+        self.module = module
+        self.setservices(servicenames)
+
+    def __del__(self):
+        self.shutdownservices()
+
+    def shutdownservices(self):
+        if len(self.allthreads) > 0:
+            rospy.loginfo('shutting down services')
+            for t in self.allthreads:
+                t.ok = False
+#                 with t.starteval:
+#                     t.starteval.notifyAll()
+            rospy.loginfo('services have shutdown')
+
+    def setservices(self,servicenames):
+        self.shutdownservices()
+        rospy.loginfo('waiting for services: %s'%servicenames)
+        for servicename in servicenames:
+            rospy.wait_for_service(servicename)
+        rospy.loginfo('starting service threads...')
+        self.allthreads = [EvaluationServerThread(rospy.ServiceProxy(name, PickledService,persistent=True), self.processResult) for name in servicenames]
+        for t in self.allthreads:
+            t.start()
+
+    def processResult(self,res):
+        if res is not None:
+            with self.evallock:
+                self.module.server_processresponse(res)
+            
+    def run():
+        starttime = time.time()
+        busythreads = self.allthreads[:]
+        # reset the evaluated threads
+        for t in self.allthreads:
+            t.starteval.acquire()
+            t.starteval.release()
+
+        num = 0
+        while True:
+            request = self.module.server_requestwork()
+            if request is None:
+                break
+            rospy.loginfo('job %d'%num)
+            service = None
+            while service == None:
+                for t in busythreads:
+                    if t.req is None:
+                        service = t
+                        break
+                if service is None:
+                    time.sleep(0.01)
+            with service.starteval:
+                service.req = PickledServiceRequest(input = pickle.dumps(request))
+                service.starteval.notifyAll()
+
+        # wait for all threads to finish
+        print 'waiting for all threads to finish'
+        for t in busythreads:
+            repeat = True
+            while(repeat):
+                with t.starteval:
+                    if t.req is None:
+                        repeat = False
+
+        print 'finished, total time: %f'%(time.time()-starttime)
+
+
+def LaunchNodes(module,serviceaddrs=[('localhost','')],rosnamespace=None,args=''):
+    nodes = """<machine name="localhost" address="localhost" default="true"/>\n"""
+    servicenames = ''
+    for i,serviceaddr in enumerate(serviceaddrs):
+        nodes += """<machine name="m%d" address="%s" default="false" %s/>\n"""%(i,serviceaddr[0],serviceaddr[1])
+        nodes += """<node machine="m%d" name="openraveservice%d" pkg="%s" type="%s" args="--startservice --module=%s --args='%s'" output="log" cwd="node">\n  <remap from="openraveservice" to="openraveservice%d"/>\n</node>"""%(i,i,PKG,sys.args[0],module.__name__,args,i)
+        servicenames += ' --service=openraveservice%d '%i
+    nodes += """<node machine="localhost" name="openraveserver" pkg="%s" type="%s" args=" --module=%s %s --args='%s'" output="screen" cwd="node"/>\n"""%(PKG,sys.args[0],module.__name__,servicenames,args)
+    xml_text = """<launch>\n"""
+    if rosnamespace is not None and len(rosnamespace) > 0:
+        xml_text += """<group ns="%s">\n%s</group>"""%(rosnamespace,nodes)
+    else:
+        xml_text += nodes
+    xml_text += "\n</launch>\n"
+    roslaunch.pmon._shutting_down = False # roslaunch registers its own signal handlers and shuts down automatically on sigints
+    launchscript = roslaunch_caller.ScriptRoslaunch(xml_text)
+    launchscript.start()
+    try:
+        starttime = time.time()
+        controlname = [name for name in launchscript.pm.get_active_names() if name.startswith('openraveserver')]
+        while True:
+            controlproc = launchscript.pm.get_process(controlname[0])
+            if controlproc is None or not controlproc.is_alive():
+                break
+            time.sleep(1)
+        print '%s finished in %ss'%(module.__name__,time.time()-starttime)
+    finally:
+        launchscript.shutdown()
+
+if __name__=='__main__':
+    parser = OptionParser(description='There are 3 modes when launching the worker: initial launch setup, server manager, service runner',
+                          usage='%prog [options]')
+    parser.add_option('--module', action='store', type='string', dest='modulename',default=None,
+                      help='module to use to execute the client/server')
+    parser.add_option('--args', action='store', type='string', dest='args',default='',
+                      help='arguments to pass in to each function')
+    parser.add_option('--startservice', action='store_true', dest='startservice',default=False,
+                      help='If set, will start a service on the ROS network offering to evaluate kinematics equations')
+    parser.add_option('--service', action='append', type='string', dest='servicenames',default=[],
+                      help='The services used to evaluate kinematics')
+    parser.add_option('--launchservice', action='append', dest='launchservices',default=[],
+                      help="""If specified, will roslaunch the services and setup the correct bindings for parallel processing (recommended). Usage: "python prog.py --launchservice='4*localhost' ...""")
+    parser.add_option('--csshgroup', action='store', type='string', dest='csshgroup',default=None,
+                      help='The group of computers to specify when launching')
+    (options, args) = parser.parse_args()
+    module=__import__(options.modulename)
+    if options.startservice:
+        module.service_start(options.args.split())
+        rospy.init_node('servicenode',anonymous=True)
+        s = rospy.Service('openraveservice', PickledService, lambda req: module.service_processrequest(pickle.loads(req.input)))
+        rospy.spin()
+    elif len(options.servicenames) > 0:
+        module.server_start(options.args.split())
+        self = EvaluationServer(module,options.servicenames)
+        self.run()
+    else:
+        module.launcher_start(options.args.split())
+        serviceaddrs = []
+        if options.csshgroup is not None:
+            infos = cpuinfos(from_cssh_file = os.path.join(os.environ["HOME"], ".cssh-clusters"), cssh_group = options.csshgroup, verbose = False, timeout = None)
+            for addr, cpuinfo in infos.items():
+                for i in range(cpuinfo[0]):
+                    serviceaddrs.append([addr,''])
+        for launchservice in options.launchservices:
+            launchservice = launchservice.strip()
+            pos = launchservice.find(' ')
+            if pos >= 0:
+                addr = launchservice[0:pos]
+                args = launchservice[pos+1:]
+            else:
+                addr = launchservice
+                args = ''
+            posnum = addr.find('*')
+            if posnum >= 0:
+                numprocesses=int(addr[0:posnum])
+                addr = addr[posnum+1:].strip()
+                for i in range(numprocesses):
+                    serviceaddrs.append([addr,args])
+            else:
+                serviceaddrs.append([addr,args])
+        LaunchNodes(module,serviceaddrs=serviceaddrs,rosnamespace=options.modulename,args=options.args)
+    sys.exit(0)
