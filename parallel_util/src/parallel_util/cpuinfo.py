4343301eb980bbc46cba5033b975cc0c95ad1985
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2011-06-30 14:15:46 +0000

    1) check a port is available or not 2) test rospack by running "rospack list"

--- parallel_util/src/parallel_util/cpuinfo.py
+++ parallel_util/src/parallel_util/cpuinfo.py
@@ -20,19 +20,21 @@ from optparse import OptionParser
 CPU_COUNT_COMMAND = 'python -c "import multiprocessing; print multiprocessing.cpu_count()"'
 MEM_COUNT_COMMAND = 'python -c "import meminfo_total; print meminfo_total.meminfo_total()"'
 ARCH_CHECK_COMMAND = 'python -c "import platform; print platform.machine()"'
-
+ROS_CHECK_COMMAND = """sh -c 'rospack list >/dev/null 2>&1 && python -c "import roslib" && echo True || echo False'"""
+ROSPORT_COMMAND = """python -c "import roslib; roslib.load_manifest('rosgraph'); import rosgraph.masterapi; print not(rosgraph.masterapi.is_online('http://localhost:%s'))" """
 def cpuinfos(hosts=[], from_cssh_file = None,
              cssh_group = None,
-             timeout = None, port = 22, verbose = False):
+             timeout = None, ros_port = 11311, verbose = False):
     if from_cssh_file and cssh_group:
         hosts = parse_cssh_config(from_cssh_file, cssh_group)
-    cpuinfos = [collect_cpuinfo(host, verbose, timeout) for host in hosts]
+    cpuinfos = [collect_cpuinfo(host, ros_port, verbose, timeout) for host in hosts]
     valid_cpuinfos = [info for info in cpuinfos if info[1] != False]
     # convert valid_cpuinfos to dict
     return_d = {}
     for info in valid_cpuinfos:
         return_d[info[0]] = [info[1], info[2], info[3]]
-    return return_d
+    port_available_p = all([info[4] == True for info in valid_cpuinfos])
+    return (return_d, port_available_p)
 
 def parse_cssh_config(config_file, group):
     infile = open(config_file, "r")
@@ -46,7 +48,7 @@ def parse_cssh_config(config_file, group):
             # matched!
             return line.split()[1:]
 
-def collect_cpuinfo(host, verbose, timeout):
+def collect_cpuinfo(host, ros_port, verbose, timeout):
     try:
         client = paramiko.SSHClient()
         client.load_system_host_keys()
@@ -69,8 +71,15 @@ def collect_cpuinfo(host, verbose, timeout):
             arch = ssh_stdout.readline().strip()
         except Exception:
             arch = False
-        return (host, cpu_num, mem_num, arch)
+        (ssh_stdin, ssh_stdout, ssh_stderr) = client.exec_command(ROS_CHECK_COMMAND)
+        ros_p = ssh_stdout.readline().strip()
+        if ros_p == "True":
+            (ssh_stdin, ssh_stdout, ssh_stderr) = client.exec_command(ROSPORT_COMMAND % (ros_port))
+            port_available = ssh_stdout.readline().strip() == "True"
+            return (host, cpu_num, mem_num, arch, port_available_p)
+        else:
+            raise "ros is not installed"
     except Exception, e:
         if verbose:
-            sys.stderr.write("[%s] connection missed\n" % (host))
+            sys.stderr.write("[%s] connection missed or ROS is not installed\n" % (host))
         return (host, False)
