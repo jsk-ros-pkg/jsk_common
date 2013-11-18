f34fbaef58b23d09824527a1d1a0cbe3436de3f4
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-11-18 12:33:08 +0000

    catkinize image_view2

---
+++ jsk_ros_patch/image_view2/catkin.cmake
@@ -0,0 +1,35 @@
+# http://ros.org/doc/groovy/api/catkin/html/user_guide/supposed.html
+cmake_minimum_required(VERSION 2.8.3)
+project(image_view2)
+
+find_package(catkin REQUIRED COMPONENTS roscpp cv_bridge std_msgs sensor_msgs geometry_msgs image_transport tf image_geometry message_filters)
+
+add_message_files(
+  FILES ImageMarker2.msg PointArrayStamped.msg
+)
+
+generate_messages(DEPENDENCIES geometry_msgs std_msgs)
+
+## 
+include_directories(include ${catkin_INCLUDE_DIRS})
+
+# Image viewers
+find_package(OpenCV REQUIRED)
+add_executable(image_view2 image_view2.cpp)
+target_link_libraries(image_view2 ${OpenCV_LIBS} ${catkin_LIBRARIES})
+add_dependencies(image_view2 ${PROJECT_NAME}_gencpp)
+
+# Point Rectangle Extractor
+add_executable(points_rectangle_extractor points_rectangle_extractor.cpp)
+find_package(Boost REQUIRED COMPONENTS signals)
+include_directories(${Boost_INCLUDE_DIRS})
+target_link_libraries(points_rectangle_extractor ${Boost_LIBRARIES} ${catkin_LIBRARIES})
+add_dependencies(points_rectangle_extractor ${PROJECT_NAME}_gencpp)
+
+catkin_package(
+    DEPENDS OpenCV
+    CATKIN-DEPENDS roscpp cv_bridge std_msgs sensor_msgs geometry_msgs image_transport tf image_geometry message_filters
+    INCLUDE_DIRS # TODO include
+    LIBRARIES # TODO
+)
+
