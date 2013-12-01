ea4e9c60650d0a7280a1455721e2ce7666bb5875
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2013-12-01 13:56:51 +0000

    update for treating multiple tf

--- jsk_topic_tools/src/specific_transform_publisher.cpp
+++ jsk_topic_tools/src/specific_transform_publisher.cpp
@@ -13,37 +13,64 @@ int main(int argc, char** argv)
 
   ros::Publisher pub_ =  pnh_.advertise<geometry_msgs::TransformStamped> ("/specific_transform", 1);
 
-  std::string parent_frame;
-  pnh_.param("parent_frame", parent_frame, std::string ("") );
+  std::vector<std::string> parent_frame;
+  std::vector<std::string> child_frame;
 
-  std::string child_frame;
-  pnh_.param("child_frame", child_frame, std::string ("") );
+  {
+    XmlRpc::XmlRpcValue param_val;
+    pnh_.getParam("parent_frame", param_val);
+    if (param_val.getType() ==  XmlRpc::XmlRpcValue::TypeArray) {
+      for(int i = 0; i < param_val.size(); i++) {
+        std::string st = param_val[i];
+        parent_frame.push_back(st);
+      }
+    } else if (param_val.getType() ==  XmlRpc::XmlRpcValue::TypeString) {
+      std::string st = param_val;
+      parent_frame.push_back(st);
+    }
+  }
+  {
+    XmlRpc::XmlRpcValue param_val;
+    pnh_.getParam("child_frame", param_val);
+    if (param_val.getType() ==  XmlRpc::XmlRpcValue::TypeArray) {
+      for(int i = 0; i < param_val.size(); i++) {
+        std::string st = param_val[i];
+        child_frame.push_back(st);
+      }
+    } else if (param_val.getType() ==  XmlRpc::XmlRpcValue::TypeString) {
+      std::string st = param_val;
+      child_frame.push_back(st);
+    }
+  }
+
+  if (parent_frame.size() != child_frame.size()) {
+    ROS_FATAL("size difference between parent frames and child frames");
+  }
 
   double loop_hz;
   pnh_.param("loop_hz", loop_hz, 1.0 );
 
-  ROS_INFO_STREAM("parent_frame:" << parent_frame);
-  ROS_INFO_STREAM("child_frame:" << child_frame);
+  for(int i = 0; i < parent_frame.size(); i++) {
+    ROS_INFO_STREAM("parent->child: " << parent_frame[i] << " -> " << child_frame[i]);
+  }
   ROS_INFO_STREAM("loop_hz:" << loop_hz);
 
   ros::Rate rate(loop_hz);
 
   while (ros::ok())
     {
-      geometry_msgs::TransformStamped tf_msg;
-      tf::StampedTransform stf;
-      try{
-      tfl_.lookupTransform(parent_frame, child_frame, ros::Time(0), stf);
-      tf::transformStampedTFToMsg(stf, tf_msg);
-
-      pub_.publish(tf_msg);
-      }catch(tf::TransformException ex){
-	ROS_INFO_STREAM("missing transform: " << parent_frame << " to " << child_frame);
+      for(int i = 0; i < parent_frame.size(); i++) {
+        geometry_msgs::TransformStamped tf_msg;
+        tf::StampedTransform stf;
+        try {
+          tfl_.lookupTransform(parent_frame[i], child_frame[i], ros::Time(0), stf);
+          tf::transformStampedTFToMsg(stf, tf_msg);
+          pub_.publish(tf_msg);
+        } catch(tf::TransformException ex) {
+          ROS_INFO_STREAM("missing transform: " << parent_frame[i] << " to " << child_frame[i]);
+        }
       }
-
       ros::spinOnce();
       rate.sleep();
     }
-
-
 }
