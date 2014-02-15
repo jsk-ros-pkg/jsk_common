a864232b8e198191d4830c218237dbb98de3a596
tnaka <nakaokat@gmail.com>
2014-02-16 03:45:49 +0900

    add patlite_node.py and launch file; patlite_node is now working

--- scripts/patlite.py
+++ scripts/patlite.py
@@ -4,23 +4,109 @@ import socket
 import binascii
 from contextlib import closing
 
-class Patlite:
-    """ This is a controller for a network patlite """
+class PatliteState(object):
+    """ This is a patlite state class """
+
+    # target
+    class Target(object):
+        LIGHT_RED = 0
+        LIGHT_YELLOW = 1
+        LIGHT_GREEN = 2
+        LIGHT_BLUE = 3
+        LIGHT_WHITE = 4
+        BUZZER = 5
+        __slots__ = []
 
     # light state
-    LIGHT_OFF = '\x00'
-    LIGHT_ON = '\x01'
-    LIGHT_FLASH_1 = '\x02'
-    LIGHT_FLASH_2 = '\x03'
-    LIGHT_REMAIN = '\x09'
+    class LightState(object):
+        OFF = '\x00'
+        ON = '\x01'
+        FLASH_1 = '\x02'
+        FLASH_2 = '\x03'
+        REMAIN = '\x09'
+        __slots__ = []
 
     # buzzer state
-    BUZZER_OFF = '\x00'
-    BUZZER_ON_1 = '\x01'
-    BUZZER_ON_2 = '\x02'
-    BUZZER_ON_3 = '\x03'
-    BUZZER_ON_4 = '\x04'
-    BUZZER_REMAIN = '\x00'
+    class BuzzerState(object):
+        OFF = '\x00'
+        ON_1 = '\x01'
+        ON_2 = '\x02'
+        ON_3 = '\x03'
+        ON_4 = '\x04'
+        REMAIN = '\x09'
+        __slots__ = []
+
+    def int2code(self, i):
+        if i == 0: return '\x00'
+        elif i == 1: return '\x01'
+        elif i == 2: return '\x02'
+        elif i == 3: return '\x03'
+        elif i == 4: return '\x04'
+        else: return '\x09'
+
+    def code2int(self, c):
+        if c == '\x00': return 0
+        elif c == '\x01': return 1
+        elif c == '\x02': return 2
+        elif c == '\x03': return 3
+        elif c == '\x04':return 4
+        else: return 9
+
+    def clear(self):
+        self.state = \
+            self.LightState.REMAIN + \
+            self.LightState.REMAIN + \
+            self.LightState.REMAIN + \
+            self.LightState.REMAIN + \
+            self.LightState.REMAIN + \
+            self.BuzzerState.REMAIN
+
+    def __init__(self, state=None):
+        if state is None:
+            self.clear()
+        else:
+            self.state = state
+
+    def red(self, value):
+        self.state = value + self.state[1:]
+        return self.state
+
+    def yellow(self, value):
+        self.state = self.state[0] + value + self.state[2:]
+        return self.state
+
+    def green(self, value):
+        self.state = self.state[:2] + value + self.state[3:]
+        return self.state
+
+    def blue(self, value):
+        self.state = self.state[:3] + value + self.state[4:]
+        return self.state
+
+    def white(self, value):
+        self.state = self.state[:4] + value + self.state[5:]
+        return self.state
+
+    def buzzer(self, value):
+        self.state = self.state[:5] + value
+        return self.state
+
+    def set_from_int(self, target, state):
+        if not self.is_valid(target, self.int2code(state)):
+            raise
+        self.state = self.state[:target] + self.int2code(state) + self.state[target+1:]
+
+    def is_valid(self, target, state):
+        # TODO to be implemented
+        return True
+
+    def __repr__(self):
+        return "<PatliteState '%s'>" % (''.join(['%x ' % ord(s) for s in self.state]))
+
+
+
+class Patlite(object):
+    """ This is a controller for a network patlite """
 
     # write command
     WRITE_HEADER = '\x58\x58\x53\x00\x00\x06'
@@ -40,16 +126,7 @@ class Patlite:
         self.timeout = timeout
         self.bufsize = 512
         self.sock = None
-        self.clearstate()
-
-    def clearstate(self):
-        self.state = \
-            self.LIGHT_REMAIN + \
-            self.LIGHT_REMAIN + \
-            self.LIGHT_REMAIN + \
-            self.LIGHT_REMAIN + \
-            self.LIGHT_REMAIN + \
-            self.BUZZER_REMAIN
+        self.state = PatliteState()
 
     def __enter__(self):
         self.open()
@@ -66,10 +143,12 @@ class Patlite:
     def close(self):
         return self.sock.close()
 
-    def write(self):
+    def write(self, state=None):
         ret = None
+        if state is None:
+            state = self.state
         try:
-            self.sock.sendall(self.WRITE_HEADER + self.state)
+            self.sock.sendall(self.WRITE_HEADER + state.state)
             ret = self.sock.recv(self.bufsize)
         except socket.timeout:
             print "timeout"
@@ -80,13 +159,14 @@ class Patlite:
     def read(self):
         try:
             self.sock.sendall(self.READ)
-            tmp_state = self.sock.recv(self.bufsize)
+            tmp_state = PatliteState(self.sock.recv(self.bufsize))
         except socket.timeout:
             print "timeout"
             return None
         return tmp_state
 
     def clear(self):
+        ret = None
         try:
             self.sock.sendall(self.CLEAR)
             ret = self.sock.recv(self.bufsize)
@@ -98,45 +178,42 @@ class Patlite:
         return True
 
     def red(self, value):
-        self.clearstate()
-        self.state = value + self.state[1:]
+        self.state.clear()
+        self.state.red(value)
         return self.write()
 
     def yellow(self, value):
-        self.clearstate()
-        self.state = self.state[0] + value + self.state[2:]
+        self.state.clear()
+        self.state.yellow(value)
         return self.write()
 
     def green(self, value):
-        self.clearstate()
-        self.state = self.state[:2] + value + self.state[3:]
+        self.state.clear()
+        self.state.green(value)
         return self.write()
 
     def blue(self, value):
-        self.clearstate()
-        self.state = self.state[:3] + value + self.state[4:]
+        self.state.clear()
+        self.state.blue(value)
         return self.write()
 
     def white(self, value):
-        self.clearstate()
-        self.state = self.state[:4] + value + self.state[5:]
+        self.state.clear()
+        self.state.white(value)
         return self.write()
 
     def buzzer(self, value):
-        self.clearstate()
-        self.state = self.state[:5] + value
+        self.state.clear()
+        self.state.buzzer(value)
         return self.write()
-
-    def print_state(self):
-        print ''.join(['%x ' % ord(s) for s in self.state])
 
 
 def test():
     patlite = Patlite("10.68.0.10")
     with patlite:
-        patlite.red(patlite.LIGHT_ON)
-        patlite.yellow(patlite.LIGHT_ON)
-        patlite.green(patlite.LIGHT_ON)
+        patlite.red(patlite.state.LightState.ON)
+        patlite.yellow(patlite.state.LightState.ON)
+        patlite.green(patlite.state.LightState.ON)
 
 if __name__ == '__main__':
     test()
