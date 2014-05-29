11d292dbabfc2596203b7fc5d0b30a52e3469094
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-05-29 10:34:57 +0900

    (#445) add  and fix setup.py

--- bayesian_belief_networks/setup.py
+++ bayesian_belief_networks/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['bayesian','bayesian_belief_networks'],
-    package_dir={'': 'lib/python2.7/dist-packages','bayesian_belief_networks':'src/bayesian_belief_networks'}
+    package_dir={'': 'lib/python2.7/dist-packages','bayesian_belief_networks':'src'}
 )
 
 setup(**d)
