cf139472223dd96c15f0bdd154656a3f7b9eb360
Ryohei Ueda <garaemon@gmail.com>
2014-09-17 23:26:05 +0900

    Add diagnostic utils from jsk_pcl_ros

--- jsk_topic_tools/catkin.cmake
+++ jsk_topic_tools/catkin.cmake
@@ -45,6 +45,8 @@ jsk_topic_tools_nodelet(src/hz_measure_nodelet.cpp
 
 add_library(jsk_topic_tools SHARED
   ${jsk_topic_tools_nodelet_sources}
+  src/timered_diagnostic_updater.cpp
+  src/diagnostic_utils.cpp
   src/rosparam_utils.cpp
   src/time_accumulator.cpp
   src/vital_checker.cpp
