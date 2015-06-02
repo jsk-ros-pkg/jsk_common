5181a39826d035043cce085e5d8583aa44a73c99
Yuto Inagaki <yuto.inagaki@gmail.com>
2015-06-02 14:55:13 -0700

    add expect of usb to check

--- jsk_tools/src/jsk_tools/sanity_lib.py
+++ jsk_tools/src/jsk_tools/sanity_lib.py
@@ -171,20 +171,27 @@ def checkNodeState(target_node_name, needed, sub_success="", sub_fail=""):
             if sub_success:
                 print Fore.GREEN+"    "+sub_success+ Fore.RESET
 
-def checkUSBExist(vendor_id, product_id, success_msg = "", error_msg = ""):
+def checkUSBExist(vendor_id, product_id, expect_usb_nums = 1, success_msg = "", error_msg = ""):
     """check USB Exists
     
     vendor_string -- vendor string (e.g. if 8087:0024, 8087)
     product_string -- product string (e.g. if 8087:0024, 0024)
+    expect_usb_nums -- number of usbs (default is 1)
     """
     output_lines = subprocess.check_output("lsusb", shell=True).split("\n")
     vendor_product = str(vendor_id) + ":" + str(product_id)
+    usb_counter = 0
     for output in output_lines:
         if vendor_product in output:
-            okMessage(vendor_product + " " + success_msg if success_msg else vendor_product + " Found")
-            return True
+            usb_counter += 1
+    if usb_counter >= expect_usb_nums:
+        okMessage(vendor_product + " " + success_msg if success_msg else vendor_product + " Found enough ( "+str(usb_counter)+"/"+str(expect_usb_nums)+" ) Detected")
+        return True
+    elif usb_counter == 0:
+        errorMessage(vendor_product + " " + error_msg if error_msg else vendor_product + " NOT Found !! ( 0 / " + str(expect_usb_nums) + " ) Detected")
+        return False
     else:
-        errorMessage(vendor_product + " " + error_msg if error_msg else vendor_product + " NOT Found !!")
+        errorMessage(vendor_product + " " + error_msg if error_msg else vendor_product + " IS FEW !! Only ( "+str(usb_counter)+"/"+str(expect_usb_nums)+" ) Detected")
         return False
             
 def checkROSMasterCLOSE_WAIT(host, username=""):
