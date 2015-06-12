21ecaaeed16714934c245a18cc22dc023039fe0f
Kei Okada <kei.okada@gmail.com>
2015-06-12 12:59:33 +0900

    split jsk_common into jsk_common_msgs and jsk_3rdparty https://github.com/jsk-ros-pkg/jsk_common/issues/919

--- hark_jsk_plugins/include/myutils.h
+++
@@ -1,5 +0,0 @@
-#ifndef __MYUTILS__
-#define __MYUTILS__
-#define FAILURE (-1)
-#define SUCCESS (0)
-#endif
--- hark_jsk_plugins/src/VERSION.cc
+++
@@ -1,5 +0,0 @@
-#include <Object.h>
-#include "../config.h"
-using namespace FD;
-DECLARE_MODULE_VERSION(HARK_JSK_VERSION, SVN_REVISION, TAGS_VERSION);
-
