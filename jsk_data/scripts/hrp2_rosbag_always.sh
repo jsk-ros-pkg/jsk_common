e2089a166fe47d5848d0b332b325bc318613e483
Ryohei Ueda <garaemon@gmail.com>
2015-08-04 17:14:58 +0900

    [jsk_tools] Record /urata_status topic in hrp2_rosbag_always.sh

--- jsk_data/scripts/hrp2_rosbag_always.sh
+++ jsk_data/scripts/hrp2_rosbag_always.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TOPICS="/joint_states /motor_states /lfsensor /rfsensor /lhsensor /rhsensor /imu /thermo_lleg /thermo_rleg /off_lfsensor /off_rfsensor /off_lhsensor /off_rhsensor /zmp /odom"
+TOPICS="/joint_states /motor_states /lfsensor /rfsensor /lhsensor /rhsensor /imu /thermo_lleg /thermo_rleg /off_lfsensor /off_rfsensor /off_lhsensor /off_rhsensor /zmp /odom /urata_status"
 # size unit is mega
 # max is 100G
 rosrun jsk_data rosbag_always.py --topics "$TOPICS" --size=100 --max-size=100000 --save-dir=$HOME/rosbags
