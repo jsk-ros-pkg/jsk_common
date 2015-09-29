88c4415eb63be5e8e7e003206314312553ebde21
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-29 13:10:09 +0900

    [jsk_data] Check existence of .ssh/config

--- jsk_data/src/jsk_data/ssh.py
+++ jsk_data/src/jsk_data/ssh.py
@@ -23,7 +23,10 @@ def _connect_ssh_context(host, username, password):
 
 
 def get_user_by_hostname(hostname):
-    with open(os.path.expanduser('~/.ssh/config')) as f:
+    ssh_config_file = os.path.expanduser('~/.ssh/config')
+    if not os.path.exists(ssh_config_file):
+        return
+    with open(ssh_config_file) as f:
         ssh_config = paramiko.util.parse_ssh_config(f)
     for entry in ssh_config._config:
         if not 'config' in entry:
