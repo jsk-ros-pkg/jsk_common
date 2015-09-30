49a43257c86e4279d9ef6fba6031bb30a4c8b9ec
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-30 17:36:38 +0900

    [jsk_topic_tools] Use ROS_INFO for warning of no connection

--- jsk_topic_tools/src/connection_based_nodelet.cpp
+++ jsk_topic_tools/src/connection_based_nodelet.cpp
@@ -63,7 +63,7 @@ namespace jsk_topic_tools
   void ConnectionBasedNodelet::warnNeverSubscribedCallback(const ros::WallTimerEvent& event)
   {
     if (!ever_subscribed_) {
-      NODELET_WARN("'%s' subscribes topics only with child subscribers.", nodelet::Nodelet::getName().c_str());
+      NODELET_INFO("'%s' subscribes topics only with child subscribers.", nodelet::Nodelet::getName().c_str());
     }
   }
 
