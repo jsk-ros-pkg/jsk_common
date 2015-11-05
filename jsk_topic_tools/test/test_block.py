77670b783fcdd56a42e624b0d437cc1354dc1acf
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-06 07:10:28 +0900

    Refactor: test_block.py as good example

--- jsk_topic_tools/test/test_block.py
+++ jsk_topic_tools/test/test_block.py
@@ -1,60 +1,62 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 import unittest
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", 
-                                                "scripts")))
 
 import rospy
 
 try:
     from std_msgs.msg import String
 except:
-    import roslib; roslib.load_manifest("jsk_topic_tools")
+    import roslib
+    roslib.load_manifest("jsk_topic_tools")
     from std_msgs.msg import String
+
+
+PKG = 'jsk_topic_tools'
+NAME = 'test_block'
 
 
 class TestBlock(unittest.TestCase):
     input_msg = None
     subscriber = None
-    def reset_subscribers(self):
-        global running
+
+    def __init__(self, *args):
+        super(TestBlock, self).__init__(*args)
+        rospy.init_node(NAME)
+        self.output_original_pub = rospy.Publisher(
+            'output_original', String, queue_size=1)
+        self.input_original_sub = rospy.Subscriber(
+            'input_original', String, self.original_topic_cb)
+
+    def original_topic_cb(self, msg):
+        self.running = True
+        self.output_original_pub.publish(msg)
+
+    def reset_subscriber(self):
         if self.subscriber:
             self.subscriber.unregister()
-            seff.subscriber = None
-        self._input_msg = None
-        running = False
+            self.subscriber = None
+        self.input_msg = None
+        self.running = False
+
+    def test_subscribe(self):
+        self.reset_subscriber()
+        self.subscriber = rospy.Subscriber("output", String, self.cb)
+        rospy.loginfo("wait 10 seconds...")
+        rospy.sleep(10)
+        self.assertFalse(self.input_msg is None)
+
     def cb(self, msg):
         self.input_msg = msg
-    def test_no_(self):
-        global running
-        # do not subscribe
-        self.reset_subscribers()
-        rospy.loginfo("wait 10 seconds...")
-        rospy.sleep(10)
-        self.assertTrue(self.input_msg == None)
-    def test_sub(self):
-        global running
-        self.reset_subscribers()
-        self.subscribe = rospy.Subscriber("/output", String, self.cb)
-        rospy.loginfo("wait 10 seconds...")
-        rospy.sleep(10)
-        self.assertFalse(self.input_msg == None)
 
-running = False
-output_original_pub = None
-def topic_cb(msg):
-    global running
-    running = True
-    output_original_pub.publish(msg)
+    def test_no_subscribe(self):
+        self.reset_subscriber()
+        # do not subscribe
+        rospy.loginfo("wait 10 seconds...")
+        rospy.sleep(10)
+        self.assertTrue(self.input_msg is None)
+
 
 if __name__ == "__main__":
     import rostest
-    rospy.init_node("test_blocke")
-    output_original_pub = rospy.Publisher("/output_original", String)
-    s = rospy.Subscriber("/input_original", String, topic_cb)
-    rostest.rosrun("jsk_topic_tools", "test_block", TestBlock)
-
+    rostest.rosrun(PKG, NAME, TestBlock)
