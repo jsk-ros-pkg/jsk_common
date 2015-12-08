38062b517582f4b0e306be4885d9a5058ea5b7aa
Ryohei Ueda <garaemon@gmail.com>
2015-12-08 15:34:05 +0900

    [jsk_tools/force_to_rename_changelog_user] Add new rule

--- jsk_tools/bin/force_to_rename_changelog_user.py
+++ jsk_tools/bin/force_to_rename_changelog_user.py
@@ -34,6 +34,7 @@ REPLACE_RULES={
     "iori": "Iori Kumagai",
     "wesleypchan": "Wesley Chan",
     "mmurooka": "Masaki Murooka",
+    "MasakiMurooka": "Masaki Murooka",
     "iory": "Iori Yanokura"}
 
 def remove_duplicates(values):
