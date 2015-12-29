dee622635bcaab46b8518bb4e31f328c2b54e0b2
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-12-29 10:36:45 +0900

    [jsk_tools/bin/battery_capacity_summary.py] more battery info

--- jsk_tools/bin/battery_capacity_summary.py
+++ jsk_tools/bin/battery_capacity_summary.py
@@ -1,51 +1,64 @@
 #!/usr/bin/env python
+import sys
 import rospy
 from std_msgs.msg import String
 from diagnostic_msgs.msg import DiagnosticArray
+try:
+    from colorama import Fore, Style, init
+except:
+  print "Please install colorama by pip install colorama"
+  sys.exit(1)
+
 
 keep_flag = True
 results = {}
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
 
 def callback(data):
     global keep_flag, results
     status = data.status
     for s in status:
         if s.name.startswith("/Power System/Smart Battery"):
-            for key_value in s.values:
-                if(key_value.key.startswith("Full Charge Capacity (mAh)")):
-                    results[s.name] = {
-                        "value":key_value.value,
-                        "hardware_id":s.hardware_id
-                        }
+            results[s.name] = { "HardwareID": s.hardware_id }
+            for kv in s.values:
+                if(kv.key.startswith("Full Charge Capacity (mAh)")):
+                    results[s.name]["FullCapacity"] = int(kv.value)
+                elif kv.key.startswith("Remaining Capacity"):
+                    results[s.name]["RemainingCapacity"] = int(kv.value)
+                elif kv.key.startswith("Batery Status"):
+                    results[s.name]["Status"] = int(kv.value)
+                elif kv.key.startswith("Cycle Count"):
+                    results[s.name]["CycleCount"] = int(kv.value)
+                elif kv.key.startswith("Manufacture Date"):
+                    results[s.name]["ManufactureDate"] = kv.value
     keep_flag = False
+
+def getColor(result):
+    cap = result["FullCapacity"]
+    if cap > 5500:
+        return Fore.GREEN
+    elif cap > 4000:
+        return Fore.YELLOW
+    else:
+        return Fore.RED
 
 def output():
     global results
-    print " |         Battery Name          | value|hardware_id|" + bcolors.ENDC
-    for key,v in results.items():
-        value = int(v["value"])
-        if value > 5500:
-            print bcolors.OKGREEN,
-        elif value > 4000:
-            print bcolors.WARNING,
-        else:
-            print bcolors.FAIL,
-        print "|" + key + "| " + str(value) + " |   " + str(v["hardware_id"]) + "   |" + bcolors.ENDC
-
+    sorted_names = sorted(results)
+    fmt = "{:>31}" + ("|{:>15}" * len(results[sorted_names[0]]))
+    sorted_keys = sorted(results[sorted_names[0]])
+    print fmt.format("Battery Name", *sorted_keys)
+    for name in sorted_names:
+        color = getColor(results[name])
+        v = [results[name][k] for k in sorted_keys]
+        print color + fmt.format(name, *v) + Fore.RESET
 
 if __name__ == '__main__':
+    init()
     rospy.init_node('battery_summary')
     rospy.Subscriber("/diagnostics_agg", DiagnosticArray, callback)
 
     while not rospy.is_shutdown() and keep_flag:
+        print "aggregating battery info..."
         rospy.sleep(1)
 
     output()
