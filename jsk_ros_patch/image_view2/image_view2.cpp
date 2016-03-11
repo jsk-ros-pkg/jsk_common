bdc15b5b8b70c22117b80defec0bf5e45f5f6825
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-03-11 15:29:34 +0900

    fix for opencv3 installed from source

--- jsk_ros_patch/image_view2/image_view2.cpp
+++ jsk_ros_patch/image_view2/image_view2.cpp
@@ -1697,17 +1697,12 @@ namespace image_view2{
   {
     if (use_window) {
       if (!window_initialized_) {
-#if CV_MAJOR_VERSION >= 3
           cv::namedWindow(window_name_.c_str(), autosize_ ? CV_WINDOW_AUTOSIZE : 0);
-#endif
-        cv::setMouseCallback(window_name_.c_str(), &ImageView2::mouseCb, this);
-#if CV_MAJOR_VERSION < 3
-        cv::namedWindow(window_name_.c_str(), autosize_ ? CV_WINDOW_AUTOSIZE : 0);
-#endif  
-        window_initialized_ = false;
+          cv::setMouseCallback(window_name_.c_str(), &ImageView2::mouseCb, this);
+          window_initialized_ = false;
       }
       if(!image_.empty()) {
-        cv::imshow(window_name_.c_str(), image_);
+          cv::imshow(window_name_.c_str(), image_);
       }
     }
   }
