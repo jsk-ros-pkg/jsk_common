791a632abcd813ae19062aa70461475c716961ab
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-07-25 16:22:35 +0900

    Sleep less time in data_collection_server.py

--- jsk_data/node_scripts/data_collection_server.py
+++ jsk_data/node_scripts/data_collection_server.py
@@ -183,7 +183,7 @@ class DataCollectionServer(object):
                         topic['name'])
                     rospy.logerr(msg)
                     return TriggerResponse(success=False, message=msg)
-            rospy.sleep(self.slop)
+            rospy.sleep(0.01)
         save_dir = osp.join(self.save_dir, str(now.to_nsec()))
         if not osp.exists(save_dir):
             os.makedirs(save_dir)
