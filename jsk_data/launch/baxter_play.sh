641e77fc9493cae8b948dbb286fb3eb758c66896
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-12-10 16:39:27 +0900

    remove tailing white space

--- jsk_data/launch/baxter_play.sh
+++ jsk_data/launch/baxter_play.sh
@@ -3,7 +3,7 @@ filenames="";
 for filename in ${@:1:$#}
 do
     filenames=$filenames" "`pwd`/$filename;
-done 
+done
 echo $filenames
 ##for check args
 ##roslaunch --args rosbag_play pr2_play.launch bagfile_names:="$filenames";
--- jsk_data/launch/hrp2_play.sh
+++ jsk_data/launch/hrp2_play.sh
@@ -3,7 +3,7 @@ filenames="";
 for filename in ${@:1:$#}
 do
     filenames=$filenames" "$filename;
-done 
+done
 echo $filenames
 ##for check args
 ##roslaunch --args rosbag_play hrp2_play.launch bagfile_names:="$filenames";
