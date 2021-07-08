c61677c4e95955a05b9d15b72141d942632a35ec
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2021-07-08 11:01:08 +0900

    fix with 2to3 -f import .

--- parallel_util/src/parallel_util/__init__.py
+++ parallel_util/src/parallel_util/__init__.py
@@ -12,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from cpuinfo import cpuinfos
+from .cpuinfo import cpuinfos
 __all__ = ["cpuinfos", "cpuinfo"]
