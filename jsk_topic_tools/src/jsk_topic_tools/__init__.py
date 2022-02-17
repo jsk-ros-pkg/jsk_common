d167b708453389d1c8898a59be00c2d1b4873ca2
iory <ab.ioryz@gmail.com>
2022-02-17 20:37:11 +0900

    [jsk_topic_tools] Add timered diagnostic updater for python

--- jsk_topic_tools/src/jsk_topic_tools/__init__.py
+++ jsk_topic_tools/src/jsk_topic_tools/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .timered_diagnostic_updater import TimeredDiagnosticUpdater
+from .vital_checker import VitalChecker
+
 from .transport import *
 from .log_utils import *
 from .name_utils import *
