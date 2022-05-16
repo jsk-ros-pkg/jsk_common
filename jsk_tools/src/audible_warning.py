51c3ee494f68741b3bee9326acb1afc2ef831b2f
iory <ab.ioryz@gmail.com>
2022-05-16 22:30:46 +0900

    [jsk_tools/audible_warning] Enable language specification

--- jsk_tools/src/audible_warning.py
+++ jsk_tools/src/audible_warning.py
@@ -97,6 +97,7 @@ class SpeakThread(Thread):
                 goal.sound_request.sound = SoundRequest.SAY
                 goal.sound_request.command = SoundRequest.PLAY_ONCE
                 goal.sound_request.arg = sentence
+                goal.sound_request.language = self.language
                 goal.sound_request.volume = self.volume
 
                 self.previous_spoken_time[e.name] = rospy.Time.now().to_sec()
