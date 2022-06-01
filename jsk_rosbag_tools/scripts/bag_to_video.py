a6169b32cb883d7cf47c4d6e569df1543302450b
iory <ab.ioryz@gmail.com>
2022-06-01 17:34:35 +0900

    [jsk_rosbag_tools] Use library's function

--- jsk_rosbag_tools/scripts/bag_to_video.py
+++ jsk_rosbag_tools/scripts/bag_to_video.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
-import datetime
-import os
-import sys
+import os.path as osp
 
-import cv2
-import moviepy.editor as mp
-from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
-
-from jsk_rosbag_tools.extract import extract_audio
-from jsk_rosbag_tools.extract import extract_image_topic
-from jsk_rosbag_tools.extract import get_image_topic_names
+from jsk_rosbag_tools.bag_to_video import bag_to_video
 
 
 def main():
     parser = argparse.ArgumentParser(description='rosbag to video')
     parser.add_argument('--out', '-o', default='',
-                        help='output directory path')
+                        help='output directory path or filename. '
+                        'If more than one --image-topic are specified, '
+                        'this will be interpreted as a directory name. '
+                        'Otherwise this is the file name.')
     parser.add_argument('--fps', default=30,
                         type=int)
     parser.add_argument('--samplerate', '-r', type=int, help='sampling rate',
@@ -25,91 +20,37 @@ def main():
     parser.add_argument('--channels',
                         type=int, default=1, help='number of input channels')
     parser.add_argument('--audio-topic', type=str, default='/audio')
-    parser.add_argument('--image-topics', type=str,
+    parser.add_argument('--image-topic', type=str, default=[],
                         nargs='+', help='Topic name to extract.')
     parser.add_argument('input_bagfile')
     args = parser.parse_args()
 
-    input_bagfile = args.input_bagfile
-    if not os.path.exists(input_bagfile):
-        print('Input bagfile {} not exists.'.format(input_bagfile))
-        sys.exit(1)
-
     if len(args.out) == 0:
-        basename = os.path.basename(input_bagfile)
-        dirname = os.path.dirname(input_bagfile)
-        filename, _ = os.path.splitext(basename)
-        out = os.path.join(dirname, filename)
+        args.out = osp.join(
+            osp.dirname(args.input_bagfile),
+            osp.splitext(osp.basename(args.input_bagfile))[0])
+    input_bagfile = args.input_bagfile
+    image_topic = None
+    image_topics = None
+    output_filepath = None
+    output_dirpath = None
+    if len(args.image_topic) == 1:
+        image_topic = args.image_topic[0]
+        output_filepath = args.out
+    elif len(args.image_topic) > 1:
+        image_topics = args.image_topic
+        output_dirpath = args.out
     else:
-        out = args.out
-    os.makedirs(out, mode=0o777, exist_ok=True)
-
-    fps = args.fps
-    dt = 1.0 / fps
-
-    wav_outpath = os.path.join(out, 'audio.wav')
-    audio_exists = extract_audio(input_bagfile, wav_outpath,
-                                 samplerate=args.samplerate,
-                                 channels=args.channels,
-                                 topic_name=args.audio_topic)
-
-    candidates_topic_names = get_image_topic_names(
-        input_bagfile, rgb_only=True)
-    if args.image_topics is None:
-        topic_names = candidates_topic_names
-    else:
-        topic_names = [tn for tn in args.image_topics
-                       if tn in candidates_topic_names]
-
-    for topic_name in topic_names:
-        images = extract_image_topic(input_bagfile, topic_name)
-
-        # remove 0 time stamp
-        stamp = 0.0
-        while stamp == 0.0:
-            stamp, _, img, _ = next(images)
-        start_stamp = stamp
-
-        if topic_name[0] == '/':
-            replaced_topic_name = topic_name[1:]
-        else:
-            replaced_topic_name = topic_name
-        replaced_topic_name = replaced_topic_name.replace('/', '--slash--')
-        output_videopath = os.path.join(
-            out, "{}.mp4".format(replaced_topic_name))
-
-        creation_time = datetime.datetime.utcfromtimestamp(start_stamp)
-        time_format = '%y-%m-%d %h:%M:%S'
-        writer = FFMPEG_VideoWriter(
-            output_videopath,
-            (img.shape[1], img.shape[0]),
-            fps, logfile=None,
-            ffmpeg_params=[
-                '-metadata',
-                'creation_time={}'.format(
-                    creation_time.strftime(time_format)),
-            ])
-
-        current_time = 0.0
-        cur_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        for stamp, _, bgr_img, _ in images:
-            rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
-            aligned_stamp = stamp - start_stamp
-            while current_time < aligned_stamp:
-                current_time += dt
-                writer.write_frame(cur_img)
-            cur_img = rgb_img
-            writer.write_frame(cur_img)
-            current_time += dt
-        writer.close()
-
-        if audio_exists:
-            output_video_with_audio_path = os.path.join(
-                out, "{}-with-audio.mp4".format(replaced_topic_name))
-            clip_output = mp.VideoFileClip(output_videopath).subclip().\
-                set_audio(mp.AudioFileClip(wav_outpath))
-            clip_output.write_videofile(
-                output_video_with_audio_path)
+        output_dirpath = args.out
+    bag_to_video(input_bagfile,
+                 output_filepath=output_filepath,
+                 output_dirpath=output_dirpath,
+                 image_topic=image_topic,
+                 image_topics=image_topics,
+                 fps=args.fps,
+                 samplerate=args.samplerate,
+                 channels=args.channels,
+                 audio_topic=args.audio_topic)
 
 
 if __name__ == '__main__':
