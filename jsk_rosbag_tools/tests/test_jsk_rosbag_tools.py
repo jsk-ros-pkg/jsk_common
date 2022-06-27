ba11a331f515b84cf11a5d3ad1c37e2dda91ebc1
iory <ab.ioryz@gmail.com>
2022-06-28 01:41:37 +0900

    [jsk_rosbag_tools] Refactor test to print command log

--- jsk_rosbag_tools/tests/test_jsk_rosbag_tools.py
+++ jsk_rosbag_tools/tests/test_jsk_rosbag_tools.py
@@ -5,6 +5,7 @@ import subprocess
 import unittest
 
 import rospkg
+import rospy
 
 
 PKG = 'jsk_rosbag_tools'
@@ -13,6 +14,17 @@ NAME = 'test_jsk_rosbag_tools'
 
 class TestJSKRosBagTools(unittest.TestCase):
 
+    def _check_command(self, cmd):
+        proc = subprocess.Popen(cmd, shell=True,
+                                stdout=subprocess.PIPE)
+        with proc.stdout:
+            for line in iter(proc.stdout.readline, b''):
+                rospy.loginfo('{}'.format(line.decode('utf-8').strip()))
+        returncode = proc.wait()
+
+        if returncode != 0:
+            raise RuntimeError('command {} failed.'.format(cmd))
+
     def test_bag_to_audio(self):
         rospack = rospkg.RosPack()
         path = rospack.get_path('jsk_rosbag_tools')
@@ -20,11 +32,7 @@ class TestJSKRosBagTools(unittest.TestCase):
                                   '20220530173950_go_to_kitchen_rosbag.bag')
         cmd = 'rosrun jsk_rosbag_tools bag_to_audio.py {}'.format(
             video_bag_path)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
     def test_tf_static_to_tf(self):
         rospack = rospkg.RosPack()
@@ -33,11 +41,7 @@ class TestJSKRosBagTools(unittest.TestCase):
                                   '20220530173950_go_to_kitchen_rosbag.bag')
         cmd = 'rosrun jsk_rosbag_tools tf_static_to_tf.py {} ' \
             '--no-progress-bar'.format(video_bag_path)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
     def test_merge(self):
         rospack = rospkg.RosPack()
@@ -48,11 +52,7 @@ class TestJSKRosBagTools(unittest.TestCase):
                                   '2022-05-07-hello-test.bag')
         cmd = 'rosrun jsk_rosbag_tools merge.py {} {} -o {}'\
             .format(audio_bag_path, audio_bag_path, output_path)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
     def test_bag_to_video_and_video_to_bag_and_compress(self):
         rospack = rospkg.RosPack()
@@ -64,22 +64,14 @@ class TestJSKRosBagTools(unittest.TestCase):
 
         cmd = 'rosrun jsk_rosbag_tools bag_to_video.py {} -o {}'.format(
             audio_bag_path, output_dir)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
         output_dir = osp.join(path, 'tests', 'output', 'video')
         video_bag_path = osp.join(path, 'samples', 'data',
                                   '20220530173950_go_to_kitchen_rosbag.bag')
         cmd = 'rosrun jsk_rosbag_tools bag_to_video.py {} -o {}'.format(
             video_bag_path, output_dir)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
         # video_to_bag.py test
         video_path = osp.join(
@@ -90,20 +82,12 @@ class TestJSKRosBagTools(unittest.TestCase):
         cmd = 'rosrun jsk_rosbag_tools video_to_bag.py {} ' \
             '--out {} --no-progress-bar'.format(
                 video_path, output_filename)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
         # compress_imgs.py test
         cmd = 'rosrun jsk_rosbag_tools compress_imgs.py {} ' \
             '--no-progress-bar'.format(output_filename)
-        proc = subprocess.Popen(cmd, shell=True)
-        proc.wait()
-
-        if proc.returncode != 0:
-            raise RuntimeError
+        self._check_command(cmd)
 
 
 if __name__ == '__main__':
