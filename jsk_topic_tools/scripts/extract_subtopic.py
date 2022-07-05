e7f256b4c8ef6834c89f387a25df5430016a7dd2
Koki Shinjo <sktometometo@gmail.com>
2022-07-05 16:39:15 +0900

    [jsk_topic_tools] support auto select for some primitive message types and update log level

--- jsk_topic_tools/scripts/extract_subtopic.py
+++ jsk_topic_tools/scripts/extract_subtopic.py
@@ -11,20 +11,34 @@ class ExtractSubtopic(object):
 
         topic = rospy.resolve_name('~input')
         real_msg_class, real_topic, msg_eval = rostopic.get_topic_class(topic)
-        rospy.logdebug('topic: {}, real_topic: {}'.format(topic, real_topic))
-        rospy.logdebug('Wating for a message from real_topic for publisher message type.')
+        rospy.loginfo('topic: {}, real_topic: {}'.format(topic, real_topic))
+        rospy.loginfo('Wating for a message from real_topic for publisher message type.')
         msg = rospy.wait_for_message(real_topic, real_msg_class)
         msg_subtopic = msg_eval(msg)
         sub_msg_class = type(msg_subtopic)
-        rospy.logdebug('publisher message type: {}'.format(sub_msg_class))
+        rospy.loginfo('subtopic message type: {}'.format(sub_msg_class))
         try:
             self.msg_eval = msg_eval
             self.pub = rospy.Publisher('~output', sub_msg_class, queue_size=1)
-            self.sub = rospy.Subscriber(real_topic, real_msg_class, self.callback)
         except ValueError:
-            rospy.logerr('Invalid subtopic message type: {}'.format(sub_msg_class))
-            sys.exit(1)
-        rospy.logdebug('initialized')
+            if sub_msg_class is str:
+                import std_msgs.msg
+                sub_msg_class = std_msgs.msg.String
+                self.pub = rospy.Publisher('~output', sub_msg_class, queue_size=1)
+                self.msg_eval_real = self.msg_eval
+                self.msg_eval = lambda msg_real: sub_msg_class(data=self.msg_eval_real(msg_real))
+            elif sub_msg_class is bool:
+                import std_msgs.msg
+                sub_msg_class = std_msgs.msg.Bool
+                self.pub = rospy.Publisher('~output', sub_msg_class, queue_size=1)
+                self.msg_eval_real = self.msg_eval
+                self.msg_eval = lambda msg_real: sub_msg_class(data=self.msg_eval_real(msg_real))
+            else:
+                rospy.logerr('Invalid subtopic message type: {}'.format(sub_msg_class))
+                sys.exit(1)
+        rospy.loginfo('Publisher message type: {}'.format(sub_msg_class))
+        self.sub = rospy.Subscriber(real_topic, real_msg_class, self.callback)
+        rospy.loginfo('initialized')
 
     def callback(self, msg):
 
