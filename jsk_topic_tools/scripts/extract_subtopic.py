27ea01a2ebad7c7820b621b46bdd3ef0ab655f7a
Koki Shinjo <sktometometo@gmail.com>
2022-07-05 16:19:56 +0900

    [jsk_topic_tools] add logprint and error handling

--- jsk_topic_tools/scripts/extract_subtopic.py
+++ jsk_topic_tools/scripts/extract_subtopic.py
@@ -2,6 +2,7 @@
 
 import rospy
 import rostopic
+import sys
 
 
 class ExtractSubtopic(object):
@@ -10,12 +11,20 @@ class ExtractSubtopic(object):
 
         topic = rospy.resolve_name('~input')
         real_msg_class, real_topic, msg_eval = rostopic.get_topic_class(topic)
+        rospy.logdebug('topic: {}, real_topic: {}'.format(topic, real_topic))
+        rospy.logdebug('Wating for a message from real_topic for publisher message type.')
         msg = rospy.wait_for_message(real_topic, real_msg_class)
         msg_subtopic = msg_eval(msg)
         sub_msg_class = type(msg_subtopic)
-        self.msg_eval = msg_eval
-        self.pub = rospy.Publisher('~output', sub_msg_class, queue_size=1)
-        self.sub = rospy.Subscriber(real_topic, real_msg_class, self.callback)
+        rospy.logdebug('publisher message type: {}'.format(sub_msg_class))
+        try:
+            self.msg_eval = msg_eval
+            self.pub = rospy.Publisher('~output', sub_msg_class, queue_size=1)
+            self.sub = rospy.Subscriber(real_topic, real_msg_class, self.callback)
+        except ValueError:
+            rospy.logerr('Invalid subtopic message type: {}'.format(sub_msg_class))
+            sys.exit(1)
+        rospy.logdebug('initialized')
 
     def callback(self, msg):
 
